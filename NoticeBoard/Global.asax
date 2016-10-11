3f08c1f24b1c4a92a8b8aa3cea4e89c2f9d935b7
lukaszwal <lukasz.wal@poczta.onet.pl>
2016-10-11 14:07:42 +0200

    Utworzenie projektu, aktualizacja paczek

---
+++ NoticeBoard/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NoticeBoard.MvcApplication" Language="C#" %>
---
+++ NoticeBoard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NoticeBoard.Startup))]
+namespace NoticeBoard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
