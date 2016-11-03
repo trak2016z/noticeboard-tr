0a910f059bd4a2a2beb9e3e60cd748a5436f6ef3
lukaszwal <lukasz.wal@poczta.onet.pl>
2016-11-03 13:01:55 +0100

    początek przenoszenia metod z AdvertisementController do Repozytorium,

---
+++ Model/Advertisements/Advertisements.cs
@@ -0,0 +1,20 @@
+﻿using Repo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo.Advertisements
+{
+    public class Advertisements
+    {
+        private NoticeBoardContext db = new NoticeBoardContext();
+           
+        public IQueryable<Advertisement> GetAdvertisements()
+        {
+            var advs = db.Advertisements.AsNoTracking();
+            return advs;
+        }
+    }
+}
--- NoticeBoard/Controllers/AdvertisementController.cs
+++ NoticeBoard/Controllers/AdvertisementController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Repo.Models;
+using Repo.Advertisements;
 using Microsoft.AspNet.Identity;
 using System.Diagnostics;
 
@@ -14,133 +15,131 @@ namespace NoticeBoard.Controllers
 {
     public class AdvertisementController : Controller
     {
-        private NoticeBoardContext db = new NoticeBoardContext();
-
+        Advertisements advRepo = new Advertisements();
         // GET: Advertisement
         public ActionResult Index()
         {
-            db.Database.Log = message => Trace.WriteLine(message);
-            var advertisements = db.Advertisements.AsNoTracking();
+            var advertisements = advRepo.GetAdvertisements();
             return View(advertisements.ToList());
         }
 
         // GET: Advertisement/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Advertisement advertisement = db.Advertisements.Find(id);
-            if (advertisement == null)
-            {
-                return HttpNotFound();
-            }
-            return View(advertisement);
-        }
+        //public ActionResult Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Advertisement advertisement = db.Advertisements.Find(id);
+        //    if (advertisement == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(advertisement);
+        //}
 
-        // GET: Advertisement/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+        //// GET: Advertisement/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
 
-        // POST: Advertisement/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [Authorize]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Description,Title,Price")] Advertisement advertisement)
-        {
-            if (ModelState.IsValid)
-            {
-                advertisement.UserId = User.Identity.GetUserId();
-                advertisement.Date = DateTime.Now;
+        //// POST: Advertisement/Create
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[Authorize]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include = "Description,Title,Price")] Advertisement advertisement)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        advertisement.UserId = User.Identity.GetUserId();
+        //        advertisement.Date = DateTime.Now;
                 
-                try
-                {
-                    db.Advertisements.Add(advertisement);
-                    db.SaveChanges();
-                    return RedirectToAction("Index");
-                }
-                catch (Exception)
-                {
-                    return View(advertisement);
-                }
+        //        try
+        //        {
+        //            db.Advertisements.Add(advertisement);
+        //            db.SaveChanges();
+        //            return RedirectToAction("Index");
+        //        }
+        //        catch (Exception)
+        //        {
+        //            return View(advertisement);
+        //        }
                 
                 
-            }
+        //    }
 
-            return View(advertisement);
-        }
+        //    return View(advertisement);
+        //}
 
-        // GET: Advertisement/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Advertisement advertisement = db.Advertisements.Find(id);
-            if (advertisement == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", advertisement.UserId);
-            return View(advertisement);
-        }
+        //// GET: Advertisement/Edit/5
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Advertisement advertisement = db.Advertisements.Find(id);
+        //    if (advertisement == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    ViewBag.UserId = new SelectList(db.Users, "Id", "Email", advertisement.UserId);
+        //    return View(advertisement);
+        //}
 
-        // POST: Advertisement/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Description,Title,Date,UserId, Price")] Advertisement advertisement)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(advertisement).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", advertisement.UserId);
-            return View(advertisement);
-        }
+        //// POST: Advertisement/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "Id,Description,Title,Date,UserId, Price")] Advertisement advertisement)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(advertisement).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    ViewBag.UserId = new SelectList(db.Users, "Id", "Email", advertisement.UserId);
+        //    return View(advertisement);
+        //}
 
-        // GET: Advertisement/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Advertisement advertisement = db.Advertisements.Find(id);
-            if (advertisement == null)
-            {
-                return HttpNotFound();
-            }
-            return View(advertisement);
-        }
+        //// GET: Advertisement/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Advertisement advertisement = db.Advertisements.Find(id);
+        //    if (advertisement == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(advertisement);
+        //}
 
-        // POST: Advertisement/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Advertisement advertisement = db.Advertisements.Find(id);
-            db.Advertisements.Remove(advertisement);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+        //// POST: Advertisement/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    Advertisement advertisement = db.Advertisements.Find(id);
+        //    db.Advertisements.Remove(advertisement);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
     }
 }
